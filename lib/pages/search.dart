1e18de3c6955c05410f2c186d8a520067f62984f
trif <trif.tz@gmail.com>
2020-03-30 23:14:32 +0200

    Improving Search Result Appearance

--- lib/pages/search.dart
+++ lib/pages/search.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -83,10 +84,11 @@ class _SearchState extends State<Search> {
           if (!snapshot.hasData) {
             return circularProgress();
           }
-          List<Text> searchResults = [];
+          List<UserResult> searchResults = [];
           snapshot.data.documents.forEach((doc) {
             User user = User.fromDocument(doc);
-            searchResults.add(Text(user.username));
+            UserResult searchResult = UserResult(user);
+            searchResults.add(searchResult);
           });
           return ListView(
             children: searchResults,
@@ -106,8 +108,40 @@ class _SearchState extends State<Search> {
 }
 
 class UserResult extends StatelessWidget {
+  final User user;
+
+  UserResult(this.user);
+
   @override
   Widget build(BuildContext context) {
-    return Text("User Result");
+    return Container(
+      color: Theme.of(context).primaryColor.withOpacity(0.7),
+      child: Column(
+        children: <Widget>[
+          GestureDetector(
+            onTap: () => print('tapped'),
+            child: ListTile(
+              leading: CircleAvatar(
+                backgroundColor: Colors.grey,
+                backgroundImage: CachedNetworkImageProvider(user.photoUrl),
+              ),
+              title: Text(
+                user.displayName,
+                style:
+                    TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+              ),
+              subtitle: Text(
+                user.username,
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ),
+          Divider(
+            height: 2.0,
+            color: Colors.white54,
+          ),
+        ],
+      ),
+    );
   }
 }
