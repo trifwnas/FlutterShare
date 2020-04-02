4cd921e876ab567eb7743dedbfd3c98175d09fb5
trif <trif.tz@gmail.com>
2020-04-03 00:42:07 +0200

    Making a Heartbeat Animation when Liking a Post

--- lib/widgets/post.dart
+++ lib/widgets/post.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+
+import 'package:animator/animator.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
@@ -76,6 +79,7 @@ class _PostState extends State<Post> {
   int likeCount;
   Map likes;
   bool isLiked;
+  bool showHeart = false;
 
   _PostState({
     this.postId,
@@ -145,6 +149,12 @@ class _PostState extends State<Post> {
         likeCount += 1;
         isLiked = true;
         likes[currentUserId] = true;
+        showHeart = true;
+      });
+      Timer(Duration(milliseconds: 500), () {
+        setState(() {
+          showHeart = false;
+        });
       });
     }
   }
@@ -156,6 +166,21 @@ class _PostState extends State<Post> {
         alignment: Alignment.center,
         children: <Widget>[
           cachedNetworkImage(mediaUrl),
+          showHeart
+              ? Animator(
+                  duration: Duration(milliseconds: 300),
+                  tween: Tween(begin: 0.8, end: 1.4),
+                  curve: Curves.elasticOut,
+                  cycles: 0,
+                  builder: (anim) => Transform.scale(
+                      scale: anim.value,
+                      child: Icon(
+                        Icons.favorite,
+                        size: 80.0,
+                        color: Colors.red,
+                      )),
+                )
+              : Text(""),
         ],
       ),
     );
