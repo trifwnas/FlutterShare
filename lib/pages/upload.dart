746a5c91459e50bf0049f1a7282ad7bc7f719fc5
trif <trif.tz@gmail.com>
2020-04-06 15:39:31 +0200

    Preserve Page State -AutomaticKeepAliveClientMixin

--- lib/pages/upload.dart
+++ lib/pages/upload.dart
@@ -21,7 +21,8 @@ class Upload extends StatefulWidget {
   _UploadState createState() => _UploadState();
 }
 
-class _UploadState extends State<Upload> {
+class _UploadState extends State<Upload>
+    with AutomaticKeepAliveClientMixin<Upload> {
   TextEditingController captionController = TextEditingController();
   TextEditingController locationController = TextEditingController();
   File file;
@@ -275,8 +276,11 @@ class _UploadState extends State<Upload> {
     locationController.text = formattedAddress;
   }
 
+  bool get wantKeepAlive => true;
+
   @override
   Widget build(BuildContext context) {
+    super.build(context);
     return file == null ? buildSplashScreen() : buildploadForm();
   }
 }
