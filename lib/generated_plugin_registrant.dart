722bda4a9381574ff23e4d0d4a0fc18a91d144e8
Trif <trif.tz@gmail.com>
2021-09-20 17:29:14 +0200

    Min

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -2,15 +2,14 @@
 // Generated file. Do not edit.
 //
 
-// ignore: unused_import
-import 'dart:ui';
+// ignore_for_file: lines_longer_than_80_chars
 
 import 'package:google_sign_in_web/google_sign_in_web.dart';
 
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 // ignore: public_member_api_docs
-void registerPlugins(PluginRegistry registry) {
-  GoogleSignInPlugin.registerWith(registry.registrarFor(GoogleSignInPlugin));
-  registry.registerMessageHandler();
+void registerPlugins(Registrar registrar) {
+  GoogleSignInPlugin.registerWith(registrar);
+  registrar.registerMessageHandler();
 }
